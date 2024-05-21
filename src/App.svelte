24c8eb52f6f3637fe7f5cd9e94ecc131a974a535
Yellowstrawberrys <dirjhhsg@gmail.com>
2024-05-21 23:27:56 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import logo from './assets/logo.svg';
+</script>
+
+<nav>
+  NAV
+</nav>
+<main>
+  <h1>Yellowstrawberry</h1>
+  <p>Student Engineer</p>
+  <br/><br/>
+  <h3>Available Languages</h3>
+  <p>: Java, Typescript, Python, C/C++, C#, Rust, SQL, Swift, Dart, Javascript, Korean, English</p>
+  <br/><br/>
+  <h3>Available Skills</h3>
+  <p>: Svelte, Quarkus, Springboot, Bukkit, Forge, Android, iOS, Embedded, Flutter, Unreal, Untiy</p>
+  <br/><br/>
+  <h3>Information</h3>
+  <p>: SRIHS 119th (Software, AnA 15th)<br/>
+    ROIN.GG Developer
+  </p>
+  <div id="cphead"></div>
+  <div id="cpbody"><div id="logo"><img src={logo} alt="logo"/><h2>&nbsp;@Yellowstrawberrys <small>(Github)</small></h2></div><div id="floppy"></div></div>
+</main>
+<nav>
+  NAV
+</nav>
